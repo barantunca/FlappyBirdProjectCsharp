96c23f13746eec48e80c4d26db03a2d606e50187
Baran Tunca <barantunca25@gmail.com>
2025-10-01 16:20:19 +0300

    Flappy Bird Project

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBird = new DevExpress.XtraEditors.PictureEdit();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pipeBot1 = new DevExpress.XtraEditors.PictureEdit();
+            this.pipeTop1 = new DevExpress.XtraEditors.PictureEdit();
+            this.picGround = new DevExpress.XtraEditors.PictureEdit();
+            this.lblScore = new DevExpress.XtraEditors.LabelControl();
+            this.pipeBot3 = new DevExpress.XtraEditors.PictureEdit();
+            this.pipeBot2 = new DevExpress.XtraEditors.PictureEdit();
+            this.pipeTop2 = new DevExpress.XtraEditors.PictureEdit();
+            this.pipeTop3 = new DevExpress.XtraEditors.PictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBird.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGround.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop3.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBird
+            // 
+            this.picBird.EditValue = global::WindowsFormsApp1.Properties.Resources.yellowbird_midflap;
+            this.picBird.Location = new System.Drawing.Point(17, 137);
+            this.picBird.Name = "picBird";
+            this.picBird.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.picBird.Properties.Appearance.Options.UseBackColor = true;
+            this.picBird.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.picBird.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.picBird.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.picBird.Size = new System.Drawing.Size(44, 33);
+            this.picBird.TabIndex = 0;
+            // 
+            // pipeBot1
+            // 
+            this.pipeBot1.EditValue = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeBot1.Location = new System.Drawing.Point(221, 291);
+            this.pipeBot1.Name = "pipeBot1";
+            this.pipeBot1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pipeBot1.Properties.Appearance.Options.UseBackColor = true;
+            this.pipeBot1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pipeBot1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pipeBot1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pipeBot1.Size = new System.Drawing.Size(59, 386);
+            this.pipeBot1.TabIndex = 3;
+            // 
+            // pipeTop1
+            // 
+            this.pipeTop1.EditValue = global::WindowsFormsApp1.Properties.Resources.pipe_green1;
+            this.pipeTop1.Location = new System.Drawing.Point(221, -142);
+            this.pipeTop1.Name = "pipeTop1";
+            this.pipeTop1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pipeTop1.Properties.Appearance.Options.UseBackColor = true;
+            this.pipeTop1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pipeTop1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pipeTop1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pipeTop1.Size = new System.Drawing.Size(59, 333);
+            this.pipeTop1.TabIndex = 4;
+            // 
+            // picGround
+            // 
+            this.picGround.EditValue = global::WindowsFormsApp1.Properties.Resources._base;
+            this.picGround.Location = new System.Drawing.Point(-8, 444);
+            this.picGround.Name = "picGround";
+            this.picGround.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.picGround.Properties.Appearance.Options.UseBackColor = true;
+            this.picGround.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.picGround.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.picGround.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.picGround.Size = new System.Drawing.Size(827, 159);
+            this.picGround.TabIndex = 7;
+            // 
+            // lblScore
+            // 
+            this.lblScore.Appearance.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Appearance.Options.UseFont = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 12);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(116, 35);
+            this.lblScore.TabIndex = 8;
+            this.lblScore.Text = "score: 0";
+            // 
+            // pipeBot3
+            // 
+            this.pipeBot3.EditValue = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeBot3.Location = new System.Drawing.Point(712, 291);
+            this.pipeBot3.Name = "pipeBot3";
+            this.pipeBot3.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pipeBot3.Properties.Appearance.Options.UseBackColor = true;
+            this.pipeBot3.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pipeBot3.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pipeBot3.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pipeBot3.Size = new System.Drawing.Size(59, 386);
+            this.pipeBot3.TabIndex = 10;
+            // 
+            // pipeBot2
+            // 
+            this.pipeBot2.EditValue = global::WindowsFormsApp1.Properties.Resources.pipe_green;
+            this.pipeBot2.Location = new System.Drawing.Point(458, 291);
+            this.pipeBot2.Name = "pipeBot2";
+            this.pipeBot2.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pipeBot2.Properties.Appearance.Options.UseBackColor = true;
+            this.pipeBot2.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pipeBot2.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pipeBot2.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pipeBot2.Size = new System.Drawing.Size(59, 386);
+            this.pipeBot2.TabIndex = 11;
+            this.pipeBot2.EditValueChanged += new System.EventHandler(this.pictureEdit2_EditValueChanged);
+            // 
+            // pipeTop2
+            // 
+            this.pipeTop2.EditValue = global::WindowsFormsApp1.Properties.Resources.pipe_green1;
+            this.pipeTop2.Location = new System.Drawing.Point(458, -142);
+            this.pipeTop2.Name = "pipeTop2";
+            this.pipeTop2.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pipeTop2.Properties.Appearance.Options.UseBackColor = true;
+            this.pipeTop2.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pipeTop2.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pipeTop2.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pipeTop2.Size = new System.Drawing.Size(59, 333);
+            this.pipeTop2.TabIndex = 12;
+            // 
+            // pipeTop3
+            // 
+            this.pipeTop3.EditValue = global::WindowsFormsApp1.Properties.Resources.pipe_green1;
+            this.pipeTop3.Location = new System.Drawing.Point(712, -142);
+            this.pipeTop3.Name = "pipeTop3";
+            this.pipeTop3.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pipeTop3.Properties.Appearance.Options.UseBackColor = true;
+            this.pipeTop3.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pipeTop3.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pipeTop3.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pipeTop3.Size = new System.Drawing.Size(59, 333);
+            this.pipeTop3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.background_day;
+            this.ClientSize = new System.Drawing.Size(810, 507);
+            this.Controls.Add(this.pipeTop3);
+            this.Controls.Add(this.pipeTop2);
+            this.Controls.Add(this.picGround);
+            this.Controls.Add(this.pipeBot2);
+            this.Controls.Add(this.pipeBot3);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pipeTop1);
+            this.Controls.Add(this.pipeBot1);
+            this.Controls.Add(this.picBird);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBird.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGround.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop3.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PictureEdit picBird;
+        private System.Windows.Forms.Timer gameTimer;
+        private DevExpress.XtraEditors.PictureEdit pipeBot1;
+        private DevExpress.XtraEditors.PictureEdit pipeTop1;
+        private DevExpress.XtraEditors.PictureEdit picGround;
+        private DevExpress.XtraEditors.LabelControl lblScore;
+        private DevExpress.XtraEditors.PictureEdit pipeBot3;
+        private DevExpress.XtraEditors.PictureEdit pipeBot2;
+        private DevExpress.XtraEditors.PictureEdit pipeTop2;
+        private DevExpress.XtraEditors.PictureEdit pipeTop3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+       
+
+        private int gravity = 1;
+        private int velocity = 0;
+
+        int score = 0;
+        bool gameOver = false;
+        Random rand = new Random();
+
+        List<DevExpress.XtraEditors.PictureEdit> tops;
+        List<DevExpress.XtraEditors.PictureEdit> bots;
+        List<bool> scored;
+
+        int pipeSpeed = 7;
+        int pipeGap = 140;
+        int pipeSpacing = 250;
+        bool gameStarted = false;
+        int highScore = 0;
+        Label lblStart;
+        Label lblHighScore;
+        private void SetupStartScreen()
+        {
+            // START yazısı
+            lblStart = new Label();
+            lblStart.Text = "FLAPPY BIRD\nBaslamak icin SPACE";
+            lblStart.Font = new Font("Arial", 24, FontStyle.Bold);
+            lblStart.ForeColor = Color.White;
+            lblStart.AutoSize = true;
+            lblStart.BackColor = Color.Transparent;
+            lblStart.Location = new Point(
+                (this.ClientSize.Width - lblStart.PreferredWidth) / 2,
+                (this.ClientSize.Height - lblStart.PreferredHeight) / 2
+            );
+            this.Controls.Add(lblStart);
+        }
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.KeyDown += Form1_KeyDown;   
+            gameTimer.Tick += gameTimer_Tick; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            
+            SetupStartScreen();
+            gameTimer.Stop(); // varsa oyun timerını başta durdur
+            
+
+
+            InitLists();
+            
+            ResetGame();
+            
+        }
+
+        private void InitLists()
+        {
+            tops = new List<DevExpress.XtraEditors.PictureEdit> { pipeTop1, pipeTop2, pipeTop3 };
+            bots = new List<DevExpress.XtraEditors.PictureEdit> { pipeBot1, pipeBot2, pipeBot3 };
+            scored = new List<bool> { false, false, false };
+        }
+
+        
+
+        private void ResetGame()
+        {
+            score = 0;
+            gameOver = false;
+            velocity = 0;
+
+            int startX = this.ClientSize.Width + 100;
+            int spacing = 300; // borular arası yatay mesafe
+            int minGapY = 100; // alt sınır
+            int maxGapY = this.ClientSize.Height - pipeGap - 100;
+            int pipeHeight = tops[0].Height; // boruların doğal yüksekliği
+
+            for (int i = 0; i < tops.Count; i++)
+            {
+                int gapY = rand.Next(minGapY, maxGapY);
+                int leftPos = startX + i * spacing;
+
+                // Üst boru
+                tops[i].Left = leftPos;
+                tops[i].Top = gapY - pipeHeight;
+
+                // Alt boru
+                bots[i].Left = leftPos;
+                bots[i].Top = gapY + pipeGap;
+
+                scored[i] = false;
+            }
+
+            // Kuşu ortala
+            picBird.Top = this.ClientSize.Height / 2 - picBird.Height / 2;
+
+            // Timer ayarı
+            gameTimer.Interval = 20;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // Kuş düşme
+            velocity += gravity;
+            picBird.Top += velocity;
+
+            // Yukarı sınır kontrolü
+            if (picBird.Top < 0)
+            {
+                EndGame();
+            }
+
+            for (int i = 0; i < tops.Count; i++)
+            {
+                tops[i].Left -= pipeSpeed;
+                bots[i].Left -= pipeSpeed;
+
+                // Boru ekrandan çıktıysa yeniden spawn (Height değişmeyecek!)
+                if (tops[i].Right < 0)
+                {
+                    int gapY = rand.Next(100, this.ClientSize.Height - pipeGap - 100);
+                    int leftPos = this.ClientSize.Width;
+
+                    tops[i].Left = leftPos;
+                    tops[i].Top = gapY - tops[i].Height; // boyutu değiştirme
+
+                    bots[i].Left = leftPos;
+                    bots[i].Top = gapY + pipeGap;
+
+                    scored[i] = false;
+                }
+
+                // Skor
+                if (!scored[i] && tops[i].Right < picBird.Left)
+                {
+                    score++;
+                    lblScore.Text = $"Score: {score}";
+                    scored[i] = true;
+                }
+
+                // Çarpışma
+                if (picBird.Bounds.IntersectsWith(tops[i].Bounds) ||
+                    picBird.Bounds.IntersectsWith(bots[i].Bounds))
+                {
+                    EndGame();
+                }
+            }
+
+            // Zemine çarpma
+            if (picBird.Top + picBird.Height >= this.ClientSize.Height)
+            {
+                EndGame();
+            }
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            gameOver = true;
+            MessageBox.Show($"Game Over! Score: {score}");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!gameStarted)
+                {
+                    // === İlk basış ===
+                    gameStarted = true;
+                    lblStart.Visible = false;   // Yazıyı kaldır
+                    gameTimer.Start();          // Timer başlasın
+                    velocity = -10;             // Kuş zıplasın (ilk jump)
+                }
+                else
+                {
+                    // === Oyun zaten başladıysa normal zıplama ===
+                    velocity = -10;
+                }
+            }
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!gameOver)
+            {
+                velocity = -10;
+            }
+        }
+
+        private void pictureEdit2_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
